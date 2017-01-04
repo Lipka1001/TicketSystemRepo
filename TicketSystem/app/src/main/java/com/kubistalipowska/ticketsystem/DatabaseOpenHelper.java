a9a9a0c1bb99c7f92fb261237a1fbe0184ad3070
Aleksander Kubista <aleksanderkubista@gmail.com>
2017-01-04 23:37:33 +0100

    db + db helpers

---
+++ TicketSystem/app/src/main/java/com/kubistalipowska/ticketsystem/DatabaseOpenHelper.java
@@ -0,0 +1,17 @@
+package com.kubistalipowska.ticketsystem;
+
+/**
+ * http://www.javahelps.com/2015/04/import-and-use-external-database-in.html
+ */
+import android.content.Context;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class DatabaseOpenHelper extends SQLiteAssetHelper {
+    private static final String DATABASE_NAME = "tickets.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+}
